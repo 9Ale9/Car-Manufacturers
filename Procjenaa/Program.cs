c5a5a671af02eda4887e1e90ba5f8e76501b0fb8
Ivan Aleric <ivanaleric77@gmail.com>
2021-11-25 16:03:26 +0100

    Add project files.

---
+++ Procjenaa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Manufacturer = "Porsche : (German)";
+            string OwnedBy = "Wolkswagen";
+            char grade = 'A';
+            int since = 1931;
+
+
+            Console.WriteLine("Porsche is graded : " + grade  +  " -Premium");
+            Console.WriteLine("Porsche's owner is : " + OwnedBy + " (German) ");
+            Console.WriteLine("Founded in : " + since + " By Ferdinand Porsche");
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
